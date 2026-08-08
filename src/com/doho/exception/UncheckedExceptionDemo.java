283c1a1c4fa57b91ff749045b61aeff0639510cb
Litudalei <gitiranjan815@gmail.com>
2026-08-08 09:10:45 +0530

    Check the Number Format

--- src/com/doho/exception/UncheckedExceptionDemo.java
+++ src/com/doho/exception/UncheckedExceptionDemo.java
@@ -1,4 +1,16 @@
 package com.doho.exception;
 
 public class UncheckedExceptionDemo {
+    public static void main(String[] args) {
+        Division d=new Division();
+        try
+        {
+            double d1=d.devide("10","0");
+            System.out.println(d1);
+        }
+        catch(ArithmeticException|NumberFormatException ex)
+        {
+            System.out.println("enter the number in valid format");
+        }
+    }
 }
